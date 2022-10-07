2fa223820622e30d50fad92e60a9f2d08c09a1fd
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2022-10-07 09:10:54 -0500

    tailwind installed

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -8,7 +8,16 @@ module.exports = {
       Tertiary: ["Poppins", "sans-serif"],
     },
     extend: {
-      
+      colors:{
+        primary: "#0F103F",
+        secondary: "#1C1C65",
+        dark: "#0f0f0f",
+        light: "#dfdfdf",
+        "blue-dark": "#2F2F8A",
+        "blue-light": "#2954A3",
+        "blue-green": "#0bfcdd",
+        "blue-skills": "rgba(11, 252, 221,.5)",
+      }
     },
   },
   plugins: [],
