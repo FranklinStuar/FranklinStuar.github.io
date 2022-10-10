ea924c21d3397c0e910b95e127086a6b540bc003
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2022-10-09 22:10:42 -0500

    index & page templates

--- page-home.php
+++ page-home.php
@@ -128,10 +128,7 @@
 </main>
 <?php endwhile?>
 <?php else:?>
-<main class="w-2/3 m-auto">
-  <h1 class="text-red text-lg">NOT CONTENT</h1>
-  <span class="text-primary">Don't can found content for this page</span>
-</main>
+  <?php get_template_part("template-parts/content", "none") ?>
 <?php endif?>
 
 <?php get_footer() ?>
---
+++ template-parts/content-none.php
@@ -0,0 +1,5 @@
+<?php if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
+<main class="w-2/3 m-auto">
+  <h1 class="text-red text-lg">NOT CONTENT</h1>
+  <span class="text-primary">Don't can found content for this page</span>
+</main>
