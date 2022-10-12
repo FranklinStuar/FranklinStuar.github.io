1222f6694c59352c396d5abdfcfed7a0261a77ba
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2022-10-11 23:45:04 -0500

    404

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
+
+<?php get_header() ?>
+
+<main>
+  <div class="container container--title">
+    <h1 class="title">404</h1>
+  </div>
+  <div class="container container--content-page">
+      CONTENT NOT FOUND
+  </div>
+</main>
+
+<?php get_footer() ?>
