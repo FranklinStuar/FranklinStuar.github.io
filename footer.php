85fcdc94a943fbb3eca8f753a6484792a1029d14
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2023-01-09 23:04:55 -0500

    fonts

--- footer.php
+++ footer.php
@@ -1,3 +1,14 @@
+<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+<script>
+  WebFont.load({
+    google: {
+      families: ['Poppins']
+    }
+  });
+</script>
+
+<script async src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<script async src="/assets/js/script.js"></script>
 
 <?php wp_footer(); ?>
 </body>
